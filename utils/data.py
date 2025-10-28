90f9d73d72a9c47b942be61abb93d899b5389720
Elena Mannoni <elemannoni04@gmail.com>
2025-10-28 15:41:36 +0100

    Create data.py

---
+++ utils/data.py
@@ -0,0 +1,34 @@
+
+def split(dataset, target, t=0.7, v=0.2, seed=42, to_torch = True):
+  
+  """
+  splits the dataset and target lists in train, val and test. 
+  if to_torch is True it also transform them into tensors
+  """
+  
+  import random
+  random.seed(42)
+  n = len(dataset)
+  n_train = int(n * t)
+  n_val = int(n * v)
+  
+  indices = list(range(n))
+  random.shuffle(indices)
+  
+  train_target = [target[i] for i in indices[:n_train]]
+  val_target = [target[i] for i in indices[n_train:n_train + n_val]]
+  test_target = [target[i] for i in indices[n_train + n_val:]]
+  
+  train_dataset = [dataset[i] for i in indices[:n_train]]
+  val_dataset = [dataset[i] for i in indices[n_train:n_train + n_val]]
+  test_dataset = [dataset[i] for i in indices[n_train + n_val:]]
+
+  if to_torch:
+    train_dataset = [torch.tensor(seq, dtype=torch.long).to(device) for seq in train_dataset]
+    val_dataset = [torch.tensor(seq, dtype=torch.long).to(device) for seq in val_dataset]
+    test_dataset = [torch.tensor(seq, dtype=torch.long).to(device) for seq in test_dataset]    
+    
+    train_target = [torch.tensor(seq, dtype=torch.long).to(device) for seq in train_target]
+    val_target = [torch.tensor(seq, dtype=torch.long).to(device) for seq in val_target]
+    test_target = [torch.tensor(seq, dtype=torch.long).to(device) for seq in test_target]
+  return train_dataset, val_dataset, test_dataset, train_target, val_target, test_target
